f15fb8a96ccb35063ca87a0ed113ef438d9109af
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-03 23:30:31 +0600

    responsive issue

--- index.js
+++ index.js
@@ -35,11 +35,7 @@ async function run() {
 
     })
 
-    app.get('/reviews',async(req,res)=>{
-      const result=await reviewsCollection.find().toArray()
-      res.send(result)
-
-    })
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
