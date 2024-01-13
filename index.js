9e9c59dd7ab4cf61ff3a311c3b5e0866400d571d
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-13 21:58:18 +0600

    create user and admin

--- index.js
+++ index.js
@@ -130,16 +130,13 @@ async function run() {
     })
     app.post('/menu', verifyToken,verifyAdmin,async(req,res)=>{
       const item=req.body
+      console.log(item)
       const result=await menuCollection.insertOne(item)
+      console.log(result)
       res.send(result)
     })
 
-    app.delete('/menu/:id', verifyToken,verifyAdmin,async(req,res)=>{
-      const id =req.params.id
-      const query={_id:new ObjectId(id)}
-      const result=await menuCollection.deleteOne(query)
-      res.send(result)
-    })
+
 
     app.get('/reviews',async(req,res)=>{
       const result=await reviewsCollection.find().toArray()
