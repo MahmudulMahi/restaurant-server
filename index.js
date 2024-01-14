be9ec1651ebda02fa30c6e9b5a997c2796c828c5
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-14 23:43:28 +0600

    create user and admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors=require('cors')
 const jwt =require('jsonwebtoken')
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -126,7 +127,14 @@ async function run() {
     app.get('/menu',async(req,res)=>{
       const result=await menuCollection.find().toArray()
       res.send(result)
+      
+    })
 
+    app.get('/menu/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:id}
+      const result=await menuCollection.findOne(query)
+      res.send(result)
     })
     app.post('/menu', verifyToken,verifyAdmin,async(req,res)=>{
       const item=req.body
@@ -135,7 +143,22 @@ async function run() {
       console.log(result)
       res.send(result)
     })
-
+    app.patch('/menu/:id',async(req,res)=>{
+      const item=req.body;
+      const id=req.params.id
+      const filter={_id:(id)}
+      const updatedDoc={
+      $set:{
+        name:item.name,
+        category:item.category,
+        price:item.price,
+        recipe:item.recipe,
+        image:item.image
+      }
+      }
+      const result=await menuCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
     app.delete('/menu/:id', verifyToken,verifyAdmin,async(req,res)=>{
       const id =req.params.id
       const query={_id:id}
@@ -167,6 +190,21 @@ async function run() {
       const query ={_id:new ObjectId(id)}
       const result =await cartCollection.deleteOne(query)
       res.send(result)
+    })
+
+    // payment
+    app.post('/create-payment-intent',async(req)=>{
+      const {price}=req.body
+      const amount=parseInt(price *100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency:'usd',
+        payment_method_types:['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
